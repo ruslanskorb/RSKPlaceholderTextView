96684e67d479098331ce7b1d750fd9597a69cbd5
Artyom Rudakov <two_dollars@icloud.com>
2020-01-15 19:19:18 +0200

    Adds Package.swfit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "RSKPlaceholderTextView",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "RSKPlaceholderTextView",
+            targets: ["RSKPlaceholderTextView"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "RSKPlaceholderTextView",
+            path: "RSKPlaceholderTextView"
+        )
+    ]
+)
